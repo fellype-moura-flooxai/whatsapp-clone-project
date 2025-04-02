c828e939da032bc4da3f303ee494a259ad1427ac
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-02 11:53:08 -0300

    feat: event to open and close the panels

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -6,6 +6,7 @@ class WhatsappController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -77,6 +78,55 @@ class WhatsappController {
         }
 
     }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+
+        });
+
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide()
+        this.el.panelEditProfile.hide();
+
+
+
+    };
 
 
 }
