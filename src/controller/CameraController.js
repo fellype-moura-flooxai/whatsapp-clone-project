4f81669841c08fe9ff925babb6fd50cd7b65e61f
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-03 12:46:37 -0300

    feat: take pictures with canvas camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -2,20 +2,45 @@ export class CameraController {
 
     constructor(videoEl){
 
-        this._video = videoEl;
+        this._videoEl = videoEl;
 
         navigator.mediaDevices.getUserMedia({
             video: true
-        }).then(steam=>{
+        }).then(stream=>{
 
-            this._videoEl.src = URL.createObjectURL(steam);
+            this._stream = stream;
+            this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
 
         }).catch(err=>{
             console.error(err);
         });
 
+    }
+
+    stop(){
+
+        this._stream.getTracks().forEach(track=>{
+            track.stop();
+        });
 
     }
+    
+    takePicture(mimeType = 'image/pgn') {
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.heifht);
+
+        return canvas.toDataURL(mimeType);
+
+    }
+
+
 
 }
